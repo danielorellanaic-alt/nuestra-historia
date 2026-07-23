6d8054fc4ab0cdba6659b01801bd3b8726bf9ef0
Daniel Orellana <daniel.orellana.ic@gmail.com>
2026-07-22 23:45:12 -0400

    Agregamos la cancion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import Hero from "./sections/Hero";
+import song from "./assets/music/cancion.mp3";
+import { useRef } from "react";
 
 function App() {
 
+  const audioRef = useRef(null);
+
   const startExperience = () => {
     console.log("Comenzó la historia ❤️");
+
+    audioRef.current.play();
   };
 
   return (
-    <Hero onStart={startExperience} />
+    <>
+      <audio 
+        ref={audioRef}
+        src={song}
+        loop
+      />
+
+      <Hero onStart={startExperience} />
+    </>
   );
 }
 
